41d27be29ae9182e67b5cf8c76065196c5aaca8f
Long Nguyen <anonyhost@gmail.com>
2021-05-11 22:03:07 +0700

    change host smt server

--- module/spell_correction.py
+++ module/spell_correction.py
@@ -1,6 +1,8 @@
 import xmlrpc.client
 
-url = 'http://104.43.10.38:9811/RPC2'
+# url = 'http://104.43.10.38:9811/RPC2'
+# url = 'http://2afbaac6632a.ngrok.io/RPC2'
+url = 'localhost:8888/RPC2'
 server = xmlrpc.client.ServerProxy(url)
 
 
